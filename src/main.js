2b28e956686791454e3d2f46b4e3223b552e8dcd
huanglei <1025925701@qq.com>
2020-07-15 17:18:35 +0800

    UI库修改

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ViewUI from 'view-design'
-import 'view-design/dist/styles/iview.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import 'normalize.css'
 import './css/commont.css'
 Vue.config.productionTip = false;
-Vue.use(ViewUI);
+Vue.use(ElementUI);
 
 
 
